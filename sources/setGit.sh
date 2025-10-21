a22f1d2579a827ebe71885c017da269412f7d31a
SerafinPL <ser.burberibar@gmail.com>
2025-10-21 17:53:37 +0200

    work with customize git cred for users are completed

--- sources/setGit.sh
+++ sources/setGit.sh
@@ -4,13 +4,13 @@
 
 make-GitCred() {
 
-
+	# $1 -login $2 -email
 	
-	git config --global user.name "SerafinPL"
+	git config --global user.name $1
 
-	git config --global user.email "ser.burberibar@gmail.com"
+	git config --global user.email $2
 
-	echo "User name and e-mail are set as SerafinPL"
+	echo "User name was set as "$1" with e-mail "$2
 }
 
 
